f1dd97a55736565410c1e99c73b7e47439f77732
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-07-13 13:51:27 -0400

    Initial Commit

---
+++ HappyTO-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyTO-App
+//
+//  Created by Yana Mayzenger on 13/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HappyTO-AppUITests/HappyTO_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HappyTO_AppUITestsLaunchTests.swift
+//  HappyTO-AppUITests
+//
+//  Created by Yana Mayzenger on 13/07/2024.
+//
+
+import XCTest
+
+final class HappyTO_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
